34b671f255e2db35a1b592d43cbf1f432510ebae
ali99944 <alitarek99944@gmail.com>
2025-04-17 07:45:35 +0200

    update

--- routes/web.php
+++ routes/web.php
@@ -6,13 +6,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-Route::get('/linkstorage', function () {
-    Artisan::call('storage:link');
-});
-
-Route::get('/create-symlink', function () {
-    symlink('/home/myusername/domains/mysub.domain.com/myappfolder/storage/app/public', '/home/myusername/domains/mysub.domain.com/public_html/storage');
-    echo "Symlink Created. Thanks";
-});
