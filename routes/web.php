d754ee6f47f69ba8abf4c848bd3619b236070efb
ali99944 <alitarek99944@gmail.com>
2025-04-17 07:40:01 +0200

    update

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/linkstorage', function () {
+    Artisan::call('storage:link');
 });
