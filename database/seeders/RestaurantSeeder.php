ea6ee3469514b6ed7e8290efaebd13d1651473f9
ali99944 <alitarek99944@gmail.com>
2025-04-17 06:54:42 +0200

    update

--- database/seeders/RestaurantSeeder.php
+++ database/seeders/RestaurantSeeder.php
@@ -32,18 +32,20 @@ class RestaurantSeeder extends Seeder
             ]
         );
 
-        Restaurants::create([
-            'id' => 1,
-            'logo' => 'images/restaurants/logos/1742943302.png',
-            'image' => 'images/restaurants/images/1742943302.png',
-            'name' => 'اسماك ناجل',
-            'description' => 'افضل انواع الاسماك',
-            'currency' => 'egp',
-            'currency_icon' => '£',
-            'code' => 'asmaak-najel',
-            'phone' => '01234567890',
-            'email' => '2kxwU@example.com',
-            'whatsapp' => '01234567890'
-        ]);
+        Restaurants::updateOrCreate(
+            ['id' => 1],
+            [
+                'logo' => 'images/restaurants/logos/1742943302.png',
+                'image' => 'images/restaurants/images/1742943302.png',
+                'name' => 'اسماك ناجل',
+                'description' => 'افضل انواع الاسماك',
+                'currency' => 'egp',
+                'currency_icon' => '£',
+                'code' => 'asmaak-najel',
+                'phone' => '01234567890',
+                'email' => '2kxwU@example.com',
+                'whatsapp' => '01234567890'
+            ]
+        );
     }
 }
