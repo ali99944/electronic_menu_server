a1e6dff6908a4c760e9e3f6a3e260f772c23edb7
ali99944 <alitarek99944@gmail.com>
2025-04-22 06:50:47 +0200

    update

--- database/migrations/2025_03_22_172943_create_restaurant_settings_table.php
+++ database/migrations/2025_03_22_172943_create_restaurant_settings_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->boolean('is_portal_active')->default(false);
             $table->boolean('is_restaurant_active')->default(false);
             $table->boolean('has_orders')->default(false);
-            $table->boolean('has_delivery')->default(false);
             $table->boolean('has_meals')->default(false);
             $table->timestamps();
         });
